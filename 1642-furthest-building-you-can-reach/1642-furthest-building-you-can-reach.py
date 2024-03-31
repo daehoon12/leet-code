d3ec530651d7ee92ea42b91bf81c33d06a7aea93
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-03-31 11:25:32 +0900

    Time: 417 ms (66.78%), Space: 31 MB (84.62%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.py
@@ -0,0 +1,15 @@
+import heapq
+
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        heap = []
+        n = len(heights)
+        for i in range(1, n):
+            jump = heights[i] - heights[i-1]
+            if jump > 0:
+                heapq.heappush(heap, jump)
+            if len(heap) > ladders:
+                bricks -= heapq.heappop(heap)
+                if bricks < 0:
+                    return i-1
+        return n-1
