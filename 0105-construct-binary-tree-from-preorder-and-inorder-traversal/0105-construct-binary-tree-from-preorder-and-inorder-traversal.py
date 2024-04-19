ec99a36eca08fd6038c9eee7b11c1a34634601e8
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-04-19 23:18:28 +0900

    Time: 83 ms (73.57%), Space: 52.8 MB (66.69%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,12 +4,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# preorder는 root를 가장 먼저 방문함. 이 성질을 이용해서 inorder에 왼쪽, 오른쪽 node를 연결할 수 있음..!
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if inorder:
-            index = inorder.index(preorder.pop(0))
-            node = TreeNode(inorder[index])
-            node.left = self.buildTree(preorder, inorder[0:index])
-            node.right = self.buildTree(preorder, inorder[index+1:])
-            
+            curr = preorder.pop(0)
+            idx = inorder.index(curr)
+            node = TreeNode(inorder[idx])
+            node.left = self.buildTree(preorder, inorder[:idx])
+            node.right = self.buildTree(preorder, inorder[idx+1:])
+
             return node
