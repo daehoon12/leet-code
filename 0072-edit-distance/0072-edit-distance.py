09e8fa4a9e52171c915ffd5f81015ba690ae7ff1
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-05-04 21:49:34 +0900

    Time: 98 ms (68.01%), Space: 20.2 MB (39.9%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,17 +1,18 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp = [[0 for _ in range(len(word2)+1)] for _ in range (len(word1)+1)]
+        word1_len, word2_len = len(word1), len(word2)
+        dp = [[0] * (word2_len + 1) for _ in range(word1_len+1)]
 
-        dp[0][0] = 0
-        
-        for i in range(1, len(word1)+1):
+        for i in range(1, word1_len + 1):
             dp[i][0] = i
-            
-        for i in range(1, len(word2)+1):
+
+        for i in range(1, word2_len + 1):
             dp[0][i] = i
-            
-        for i in range(1, len(word1)+1):
-            for j in range(1, len(word2)+1):
-                if word1[i-1] == word2[j-1]: dp[i][j] = dp[i-1][j-1]
-                else: dp[i][j] = min(dp[i-1][j-1] + 1, dp[i-1][j] + 1, dp[i][j-1] + 1)
-        return dp[len(word1)][len(word2)]
+
+        for i in range(1, word1_len + 1):
+            for j in range(1, word2_len+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1]) + 1
+        return dp[word1_len][word2_len]
