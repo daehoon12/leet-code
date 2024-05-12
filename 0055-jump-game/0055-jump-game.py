c27a1eba76cd899fc49d9104d39fb818991746b2
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-05-12 14:45:09 +0900

    Time: 382 ms (28.46%), Space: 17.8 MB (87.58%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,18 +1,12 @@
-# 뛸 수 있는 step을 갱신해주면 어떨까?
-# 현재 가리키고 있는 step의 값과 현재 뛸수 있는 step 값 중 큰 값을 계속 갱신하면 될듯..?
-
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        curr_step = nums[0]
-        for idx,step in enumerate(nums):
+        step = nums[0]
+        for idx, val in enumerate(nums):
             if idx == len(nums) -1:
                 return True
-
-            curr_step -= 1
-            curr_step = max(curr_step, step)
-            
-            if curr_step <= 0:
+            step = max(val, step)
+            if step <= 0:
                 return False
-
-        return True                
-        
+            step -= 1
+            
+        return True
