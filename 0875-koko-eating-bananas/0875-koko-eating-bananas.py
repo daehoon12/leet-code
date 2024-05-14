e7a6c20b7273c7ec8eff3bdf264e053148105d3c
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-05-14 18:09:35 +0900

    Time: 344 ms (8.93%), Space: 17.9 MB (98.93%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,19 +1,22 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def check(mid,h, piles):
+        def check(mid):
             cnt = 0
             for p in piles:
                 if p % mid == 0:
-                    cnt += (p // mid)
+                   cnt += p // mid
                 else:
-                    cnt += (p // mid) + 1
-            return cnt <=h
-            
-        low, high = 1, max(piles)
+                    cnt += p // mid + 1
+            return cnt > h
+
+        low, high = 1, sum(piles)
+        
         while low < high:
             mid = (low + high) // 2
-            if (check(mid, h, piles)):
-                high = mid
-            else:
+            if check(mid):
                 low = mid + 1
+            else:
+                high = mid
         return high
+
+            
