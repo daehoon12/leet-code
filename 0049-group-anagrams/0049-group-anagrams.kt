920fa39e321813cf98dd3038f4cf621173ba5790
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-05-31 15:37:41 +0900

    Time: 358 ms (67.26%), Space: 44.3 MB (64.4%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        val groups: MutableMap<String, MutableList<String>> = mutableMapOf()
+
+        for (str in strs){
+            val key = str.toCharArray().sorted().joinToString("")
+            groups.getOrPut(key){ mutableListOf() } 
+            groups[key]!!.add(str)
+        }
+        
+        return ArrayList<List<String>>(groups.values)
+    }
+}
