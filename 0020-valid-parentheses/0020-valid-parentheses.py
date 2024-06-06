2ebc5a1000084595b05bae7c611205ed389d873c
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-06-06 13:51:19 +0900

    Time: 23 ms (98.77%), Space: 16.4 MB (99.54%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(len(s)):
             if not dic.get(s[i]):
                 stack.append(s[i])
-            elif not stack and dic.get(s[i]):
+            elif not stack or dic.get(s[i]) != stack[-1]:
                 return False
             elif stack and dic.get(s[i]) == stack[-1]:
                 stack.pop()
