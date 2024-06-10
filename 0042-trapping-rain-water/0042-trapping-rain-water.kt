2bf011eaf841d56f432c660805da3597556b295a
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-06-10 14:20:31 +0900

    Time: 185 ms (95.25%), Space: 40.4 MB (45.48%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.kt
+++ 0042-trapping-rain-water/0042-trapping-rain-water.kt
@@ -2,21 +2,25 @@ class Solution {
     fun trap(height: IntArray): Int {
         var left = 0
         var right = height.size -1
-        var maxLeft = height[left]
-        var maxRight = height[right]
+        
+        var leftMax = height[left]
+        var rightMax = height[right]
+
         var answer = 0
         while (left < right){
-            if (maxLeft < maxRight){
-                answer += maxLeft - height[left] 
-                left++
-            } else{
-                answer += maxRight - height[right]
-                right--
+            when {
+                leftMax < rightMax -> {
+                    answer += leftMax - height[left]
+                    left++
+                    leftMax = max(leftMax, height[left])
+                }
+                else -> {
+                    answer += rightMax - height[right]
+                    right--
+                    rightMax = max(rightMax, height[right])
+                }
             }
-            maxLeft = max(maxLeft, height[left])
-            maxRight = max(maxRight, height[right])
         }
-        
         return answer
     }
 }
