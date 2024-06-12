c81944fad544cd9ef9d4606f84c3f5c586b4f8fc
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-06-12 21:39:53 +0900

    Time: 46 ms (16.29%), Space: 16.5 MB (82.42%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,16 +1,16 @@
-from collections import Counter
+from collections import Counter, defaultdict
 
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         count = Counter(s)
         stack = []
-
+        seen = defaultdict(lambda: False)
         for e in s:
             count[e] -= 1
             if e in stack:
                 continue
-                
+
             while stack and e < stack[-1] and count[stack[-1]] > 0:
-                stack.pop()
+                seen[stack.pop()] = True
             stack.append(e)
         return "".join(stack)
