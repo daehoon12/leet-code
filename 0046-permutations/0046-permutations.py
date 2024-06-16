10f00c8977174eac6c2bf473026f3f94224b267b
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-06-16 15:16:43 +0900

    Time: 40 ms (59.75%), Space: 16.8 MB (8.57%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,20 +1,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        answer2 = []
-        answer = []
+        ans = []
+        n = len(nums)
         visited = [False] * len(nums)
-        def dfs(cnt):
-            if cnt == len(nums):
-                answer2.append(copy.deepcopy(answer))
-                return 
-
-            for i in range(0, len(nums)):
+        def backtracking(cnt, tmp):
+            if n== cnt:
+                ans.append(tmp.copy())
+                return
+            
+            for i in range(0, n):
                 if not visited[i]:
                     visited[i] = True
-                    answer.append(nums[i])
-                    dfs(cnt+1)
-                    answer.pop()
+                    tmp.append(nums[i])
+                    backtracking(cnt +1, tmp)
+                    tmp.pop()
                     visited[i] = False
-        dfs(0)
-        return answer2
-            
+
+        backtracking(0,[])
+        return ans
