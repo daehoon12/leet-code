03b07b75a993a92f5726313dea6d0a3ea3e664b2
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-06-17 14:08:50 +0900

    Time: 54 ms (51.82%), Space: 16.5 MB (73.2%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        answer=[]
-        def dfs(idx, element, summ):
-            if summ == target:
-                if sorted(element) not in answer:
-                    answer.append(element)
-            if summ > target:
+        def dfs(start, path, target):
+            if target == 0:
+                result.append(path)
+                return
+            if target < 0:
                 return
             
-            for i in range(idx, len(candidates)):
-                dfs(i, element+[candidates[i]], summ+candidates[i])
-               
-        candidates=sorted(candidates)
-        dfs(0,[],0)
-        return answer
+            for i in range(start, len(candidates)):
+                # 현재 후보군을 포함한 새로운 경로와 갱신된 타겟 값으로 dfs 재귀 호출
+                dfs(i, path + [candidates[i]], target - candidates[i])
         
+        candidates.sort()  # 후보군 정렬
+        result = []
+        dfs(0, [], target)
+        return result
