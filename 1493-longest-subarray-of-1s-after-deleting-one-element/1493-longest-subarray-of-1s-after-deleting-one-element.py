2b12b880c7da631fe1d0bfba9d129280880e9f29
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-06-19 22:33:04 +0900

    Time: 491 ms (10.42%), Space: 19.6 MB (64.98%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,23 +1,22 @@
+from collections import defaultdict
+
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        if nums.count(1) == len(nums):
-            return len(nums)-1
-        low, high = 0 ,0
-        nums_len = len(nums)
-        deleted = False
-        answer, feasible = 0, 0
-        while high < nums_len:
-            if nums[high] == 1:
-                feasible += 1
-                high += 1
-            elif nums[high] == 0 and not deleted:
-                deleted = True
-                high += 1
-            elif nums[high] == 0 and deleted:
-                if nums[low] == 0:
-                    deleted = False
-                elif nums[low] == 1:
-                    feasible -= 1
-                low += 1
-            answer = max(answer, feasible)
-        return answer
+        left = 0
+        answer = 0
+        zeros = 0
+        for right, num in enumerate(nums):
+            if num == 0:
+                zeros += 1
+            while zeros > 1:
+                if (nums[left] == 0):
+                    zeros -= 1
+                left += 1
+            
+            answer = max(answer, right - left + 1 - zeros)
+        return answer -1 if answer == len(nums) else answer
+            
+            
+
+
+        
