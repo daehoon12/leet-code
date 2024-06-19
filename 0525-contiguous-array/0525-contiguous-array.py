3dbe7157aac931f6c94183e7ec66df59e5ca7753
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-06-19 14:22:58 +0900

    Time: 608 ms (78.45%), Space: 22.2 MB (55.15%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        answer = 0
+        start = 0
+        n = len(nums)
+        dic = defaultdict(int)
+        total = 0
+        dic[0] = -1
+        for i, num in enumerate(nums):
+            if num == 1:
+                total += 1
+            else:
+                total -= 1
+
+            if total in dic:
+                answer = max(answer, i - dic[total])
+            else:
+                dic[total] = i        
+        return answer
+
+                
