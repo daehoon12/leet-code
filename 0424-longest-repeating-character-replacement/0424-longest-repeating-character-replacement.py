951b962e2371207e7531720cb2e958d195b00b1d
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-07-03 12:23:53 +0900

    Time: 193 ms (9.79%), Space: 16.4 MB (94.64%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -4,14 +4,18 @@ class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = Counter()
         left, answer = 0, 0
-        for right, char in enumerate(s):
-            count[char] += 1
+
+        for right, val in enumerate(s):
+            count[val] += 1
             max_char_n = count.most_common()[0][1]
-            
-            if right - left + 1 - max_char_n > k:
+
+            if right - left - max_char_n + 1 > k:
                 count[s[left]] -= 1
                 left += 1
-                
             answer = max(answer, right - left + 1)
-                
         return answer
+
+                    
+        
+            
+        
