b2d2098151ba7eec93c7c4670d546d82ff3d30ac
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-07-11 16:09:13 +0900

    Time: 67 ms (46.87%), Space: 17.1 MB (57.53%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        st = []
+        operators = {"+", "-", "*", '/'}
+        for token in tokens:
+            if token not in operators:
+                st.append(int(token))
+            else:
+                second = st.pop()
+                first = st.pop()
+                
+                if token == '+':
+                    result = first + second
+                elif token == '-':
+                    result = first - second
+                elif token == '*':
+                    result = first * second
+                elif token == '/':
+                    result = int(math.trunc(first / second))
+                st.append(result)
+        return st[0]
