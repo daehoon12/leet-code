005b64fb3873cc8f309783a75669de40f3fe23fd
DaeHoon <32921115+daehoon12@users.noreply.github.com>
2024-07-24 18:26:35 +0900

    Time: 90 ms (55.69%), Space: 18.3 MB (42.12%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,30 +2,31 @@ from collections import defaultdict
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = collections.defaultdict(list)
-        for x, y in prerequisites:
-            graph[x].append(y)
-            
-        traced =set()
-        visited=set()
-        
-        def dfs(start):
-            if start in traced:
+        def dfs(curr):
+            if curr in traced:
                 return False
-            if start in visited:
+            if curr in visited:
                 return True
-          
-            traced.add(start)
-            for i in graph[start]:
-                if not dfs(i):
+
+            traced.add(curr)
+            for next_node in graph[curr]:                    
+                if not dfs(next_node):
                     return False
-            traced.remove(start)
-            visited.add(start)
+            traced.remove(curr)
+            visited.add(curr)
             return True
-        
+
+        graph = defaultdict(list)        
+        visited = set()
+        traced = set()
+        for s,e in prerequisites:
+            graph[e].append(s)
+
         for i in list(graph):
             if not dfs(i):
-                return False
+                return False 
+        return True   
+
+                
+
             
-        return True
-                    
